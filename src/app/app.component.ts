9e5644ad3a8be7a3565337c6a07f06e95afeb4ed
kavijarr <kavijakumuditha12@gmail.com>
2024-06-09 09:51:42 +0530

    navbar-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,HomeComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
