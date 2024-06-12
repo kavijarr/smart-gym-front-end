1f033fcf5bb2026ed3c1fd1594fe87dda2e9a594
kavijarr <kavijakumuditha12@gmail.com>
2024-06-13 00:12:09 +0530

    sign-in form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 export const routes: Routes = [
   {
     path:"",
     component:HomeComponentComponent
   },
   {
-    path:"admin",
-    component:AdminDashboardComponent
+    path:"signin",
+    component:SignInComponent
   }
 ];
