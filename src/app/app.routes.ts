cb05a8949bd3fc79a410507ccb1541fea095d68b
Thisumitha <thisu2006@gmail.com>
2024-06-13 00:20:19 +0530

    12.20

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { CustomerRegistrationComponent } from './components/customer-registration/customer-registration.component';
 export const routes: Routes = [
   {
     path:"",
@@ -9,5 +10,9 @@ export const routes: Routes = [
   {
     path:"admin",
     component:AdminDashboardComponent
+  },
+  {
+    path:"customer-reg",
+    component:CustomerRegistrationComponent
   }
 ];
